3f05b5ce538dce898b532a6c346f36a968bda194
Sean Jaculina <sjaculina@horizon.csueastbay.edu>
2021-09-21 09:00:14 -0700

    edits to app and form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import Post from './components/Post';
+import Postform from './components/Postform';
 import './App.css';
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Postform />
+      <Post />
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentWillMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          posts: data,
+        })
+      );
+  }
+
+  render() {
+    let postItems = this.state.posts.map((post) => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
+
+    return <div>{postItems}</div>;
+  }
+}
+
+export default Post;
